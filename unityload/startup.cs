0250c49c666da33bb855687cf7c0d7e1504fcc1c
BitsByWill <8272056+BitsByWill@users.noreply.github.com>
2019-02-26 23:01:17 -0800

    details for startup

---
+++ unityload/startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+public class 
+{
+	// remember to start windows hidden
+	public static string xyStart; //once close to finish, compile all scripts into one exe
+	public static string zStart;
+	public static string handStart;
+	public static string calibrateStart1;
+	public static string calibrateStart2;
+	public static string destroyProcesses; //keep as batch script?
+	
+	public static void start()
+	{
+		System.Diagnostics.Process.Start("CMD.exe", calibrateStart1);
+		System.Diagnostics.Process.Start("CMD.exe", calibrateStart2);
+		System.Diagnostics.Process.Start("CMD.exe", xyStart);
+		System.Diagnostics.Process.Start("CMD.exe", zStart);
+		System.Diagnostics.Process.Start("CMD.exe", handStart);
+	}
+	
+	public static destroy()
+	{
+		System.Diagnostics.Process.Start("CMD.exe", destroyProcesses);
+	}
+}
