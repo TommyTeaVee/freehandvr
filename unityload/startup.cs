a15c9a35868c12f44aac3044e42734f3c49108c8
BitsByWill <8272056+BitsByWill@users.noreply.github.com>
2019-02-26 23:10:21 -0800

    add end process

--- unityload/startup.cs
+++ unityload/startup.cs
@@ -7,6 +7,9 @@ public class
 	public static string xyStart; //once close to finish, compile all scripts into one exe
 	public static string zStart;
 	public static string handStart;
+	public static string xyStartName;
+	public static string zStartName;
+	public static string handStartName;
 	public static string calibrateStart1;
 	public static string calibrateStart2;
 	public static string destroyProcesses; //keep as batch script?
@@ -22,6 +25,18 @@ public class
 	
 	public static destroy()
 	{
-		System.Diagnostics.Process.Start("CMD.exe", destroyProcesses);
+		//System.Diagnostics.Process.Start("CMD.exe", destroyProcesses); below idea might work better, needs to be tested
+		foreach (var process in Process.GetProcessesByName(xyStartName))
+		{
+			process.Kill();
+		}
+		foreach (var process in Process.GetProcessesByName(zStartName))
+		{
+			process.Kill();
+		}
+		foreach (var process in Process.GetProcessesByName(handStartName))
+		{
+			process.Kill();
+		}
 	}
 }
