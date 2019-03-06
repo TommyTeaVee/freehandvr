ae102ae37c12a570a36d7b39b94770084a7eec2f
BitsByWill <8272056+BitsByWill@users.noreply.github.com>
2019-03-05 22:20:11 -0800

    improve gui interface for calibration

--- calibration/gui.cpp
+++ calibration/gui.cpp
@@ -13,17 +13,21 @@ int calibrate(info *data) //return 1 to stop the same interface from repeating
 	int highv = 255;
 	int key = 0;
 	VideoCapture source(data->camera); //start camera from camera number defined in structs
-	namedWindow("Calibration", CV_WINDOW_AUTOSIZE); //create calibration window
+	namedWindow("FreeHandVR Calibration", CV_WINDOW_NORMAL); //create calibration window
 	//Create trackbars, usually low changing values is sufficient
-	cvCreateTrackbar("LowH", "Calibration", &lowh, 179); //Hue (0 - 179)
-	cvCreateTrackbar("LowS", "Calibration", &lows, 255); //Saturation (0 - 255)
-	cvCreateTrackbar("LowV", "Calibration", &lowv, 255); //Value (0 - 255)
+	cvCreateTrackbar("LowH", "FreeHandVR Calibration", &lowh, 179); //Hue (0 - 179)
+	cvCreateTrackbar("LowS", "FreeHandVR Calibration", &lows, 255); //Saturation (0 - 255)
+	cvCreateTrackbar("LowV", "FreeHandVR Calibration", &lowv, 255); //Value (0 - 255)
+	source.read(mainImage); //quick read to figure out rectangle size for gaussian blur to display text
+	Rect region(0, 0, mainImage.cols, 140);
 	while (cont)
 	{
 		source.read(mainImage);
 		//creating HSV image
 		cvtColor(mainImage, hsvImage, COLOR_BGR2HSV);
 		inRange(hsvImage, Scalar(lowh, lows, lowv), Scalar(highh, highs, highv), thresholdedImage);
+		//blur top part of original image to display instructions
+		GaussianBlur(mainImage(region), mainImage(region), Size(0, 0), 6);
 		//morphological opening
 		erode(thresholdedImage, thresholdedImage, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 		dilate(thresholdedImage, thresholdedImage, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
@@ -33,8 +37,14 @@ int calibrate(info *data) //return 1 to stop the same interface from repeating
 		//convert back to same type
 		cvtColor(thresholdedImage, thresholdedImage, CV_GRAY2BGR);
 		hconcat(mainImage, thresholdedImage, combinedView);
-		//show windows
-		imshow("Calibration View", combinedView);
+		//add instructions for usage
+		putText(combinedView, "Adjust the Values", cvPoint(20, 20), FONT_HERSHEY_COMPLEX_SMALL, 0.75, cvScalar(0, 0, 255), 1, CV_AA);
+		putText(combinedView, "until you see an Optimal Circle ->", cvPoint(20, 40), FONT_HERSHEY_COMPLEX_SMALL, 0.75, cvScalar(0, 0, 255), 1, CV_AA);
+		putText(combinedView, "ESC - exit", cvPoint(20, 60), FONT_HERSHEY_COMPLEX_SMALL, 0.7, cvScalar(0, 0, 255), 1, CV_AA);
+		putText(combinedView, "N - next cam", cvPoint(20, 80), FONT_HERSHEY_COMPLEX_SMALL, 0.7, cvScalar(0, 0, 255), 1, CV_AA);
+		putText(combinedView, "B - prev cam", cvPoint(20, 100), FONT_HERSHEY_COMPLEX_SMALL, 0.7, cvScalar(0, 0, 255), 1, CV_AA);
+		putText(combinedView, "Enter - confirm values", cvPoint(20, 120), FONT_HERSHEY_COMPLEX_SMALL, 0.7, cvScalar(0, 0, 255), 1, CV_AA);
+		imshow("Calibration View (FreeHandVR)", combinedView);
 		key = waitKey(20);
 		if ((key == 27) || (key == 110) || (key == 98)|| (key == 13)) //ESC, n, b, return
 		{
