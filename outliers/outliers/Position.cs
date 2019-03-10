f45f8b3378e6851490a8c7a1e08f62da50f7e069
Kosi Nwabueze <kosinwabueze@gmail.com>
2019-03-09 23:46:37 -0800

    Finished parsing outliers into C#

---
+++ outliers/outliers/Position.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace outliers
+{
+    public struct Position
+    {
+        public double X;
+        public double Y;
+        public double Depth;
+    }
+}
---
+++ outliers/outliers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace outliers
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("dataForKosi.txt"))
+            {
+                Position[] t = ParseData(sr);
+                
+                ;
+            }
+        }
+
+        public static Position[] ParseData(StreamReader sr)
+        {
+            Regex rx = new Regex(@".*?(\d+), (\d+).*?", RegexOptions.Compiled);
+
+            List<Position> pos = new List<Position>();
+
+            string line;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (line.Contains("Red"))
+                {
+                    Match mt = rx.Match(line);
+
+                    Position xy = new Position();
+
+                    xy.X = double.Parse(mt.Groups[1].Value);
+                    xy.Y = double.Parse(mt.Groups[2].Value);
+
+                    pos.Add(xy);
+                }
+            }
+
+            return pos.ToArray();
+        }
+    }
+}
